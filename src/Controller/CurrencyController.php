5ee61ba8556924ce36973f7487aaf2cb6efc3c74
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-05-22 10:56:59 +0700

    install web profiler

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -3,20 +3,19 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CurrencyController
+class CurrencyController extends AbstractController
 {
     /**
      * @Route("/",name="homepage")
-     *
-     * @return Response
      */
     public function homepage()
     {
-        return new Response("OMG my first page already!!");
+        return $this->render("currency/homepage.html.twig",[]);
     }
 
     /**
